a406ab87723d6cee0478aba1040a71c63592668e
ignatova-e <evgeniiaignatova0@gmail.com>
2026-04-03 23:30:52 +0300

    debug

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import './quest-laptop.js'
-import './planning.js'
-import './quest-engine.js'
-
 import Phaser from 'phaser'
 import { scenarios } from './scenarios.js'
 // Делаем данные сценариев доступными глобально для GameScene
