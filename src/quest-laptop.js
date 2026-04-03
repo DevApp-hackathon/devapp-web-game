c2494999a045fe32b2ff6a0255482629a60218e1
ignatova-e <evgeniiaignatova0@gmail.com>
2026-04-03 23:19:15 +0300

    debug

--- src/quest-laptop.js
+++ src/quest-laptop.js
@@ -449,7 +449,8 @@ if (document.readyState === 'loading') {
 
 window.addEventListener('keydown', (e) => {
   if (e.key.toLowerCase() === 'e') {
-    // чтобы не открывалось 100 раз
+    if (typeof window.openLaptopQuest !== 'function') return
+
     const overlay = document.getElementById('quest-overlay')
     if (overlay && overlay.style.display !== 'flex') {
       window.openLaptopQuest()
